1db070d789fbde2d629fed10b24be13b830515f0
Roland Branten <roland.branten@gmail.com>
2021-12-27 21:57:55 +0100

    fix ts version bug

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,19 +2,19 @@
 import { GetServerSideProps } from 'next';
 import { getSession, signIn } from 'next-auth/react';
 import React from 'react';
-import { wrapper, left, right, red } from 'components/login/Login.module.scss';
+import styles from 'components/login/Login.module.scss';
 import Image from 'next/image';
 import Button from 'global_components/Button/Button';
 // =========================
 
 export default function Login() {
   return (
-    <div className={wrapper}>
-      <div className={left}>
-        <div className={red} />
+    <div className={styles.wrapper}>
+      <div className={styles.left}>
+        <div className={styles.red} />
         <Image src="/images/auth-image.svg" layout="fill" alt="auth" />
       </div>
-      <div className={right}>
+      <div className={styles.right}>
         <h1>Login</h1>
         <Button color="white" onClick={() => signIn('google')}>
           <Image src="/images/google.svg" width="16" height="16" />

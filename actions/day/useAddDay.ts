54684a2141db2103cd6e38ebb04725c7fd67244d
Roland Branten <roland.branten@gmail.com>
2022-01-02 19:52:34 +0100

    update day invalidQuery calls

--- actions/day/useAddDay.ts
+++ actions/day/useAddDay.ts
@@ -1,16 +1,13 @@
 // Components==============
 import axios from 'axios';
 import { useMutation, useQueryClient } from 'react-query';
-import { getDayString } from 'utils/getDayString';
 // =========================
 
 export default function useAddDay() {
   const queryClient = useQueryClient();
-  const today = new Date();
-  const key = getDayString(today);
 
   const mutation = useMutation(() => axios.post('/api/day'), {
-    onSuccess: () => queryClient.invalidateQueries(['days', key]),
+    onSuccess: () => queryClient.invalidateQueries('days'),
   });
 
   return mutation;

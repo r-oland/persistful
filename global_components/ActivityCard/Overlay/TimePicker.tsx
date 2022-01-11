f830330693ce004d146a4b778499fd6154a1e327
Roland Branten <roland.branten@gmail.com>
2022-01-11 21:53:18 +0100

    finish timePicker component

--- global_components/ActivityCard/Overlay/TimePicker.tsx
+++ global_components/ActivityCard/Overlay/TimePicker.tsx
@@ -1,22 +1,30 @@
 // Components==============
 import { motion, useAnimation, useMotionValue } from 'framer-motion';
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import styles from './Overlay.module.scss';
 // =========================
 
 export default function TimePicker({
   values,
   setState,
+  times,
+  resetEffect,
 }: {
   values: string[];
   setState: React.Dispatch<React.SetStateAction<number>>;
+  times?: boolean;
+  resetEffect: boolean;
 }) {
   const controls = useAnimation();
   const valuesCount = 30;
-  const amountOfValues = Array.from(Array(valuesCount).keys());
-  const startPos = values.length * -20 * (valuesCount / 2);
+  const amountOfValues = times ? [0] : Array.from(Array(valuesCount).keys());
+  const startPos = times ? 0 : values.length * -20 * (valuesCount / 2);
 
   const y = useMotionValue(startPos);
+
+  useEffect(() => {
+    controls.start({ y: startPos });
+  }, [resetEffect]);
 
   return (
     <motion.div
@@ -24,6 +32,9 @@ export default function TimePicker({
       drag="y"
       animate={controls}
       style={{ y }}
+      dragConstraints={
+        times ? { bottom: 0, top: (values.length - 1) * -20 } : undefined
+      }
       onDragEnd={(c, i) => {
         const getNearestValue = () => {
           // Get nearest integer
@@ -44,11 +55,20 @@ export default function TimePicker({
           return nearestVal;
         };
 
-        // set position based on velocity
-        const delay =
-          (i.velocity.y < 0 ? Math.abs(i.velocity.y) : i.velocity.y) * 3;
+        // get delay based on velocity
+        const delay = () => {
+          // get velocity as a positive value;
+          let velocity =
+            i.velocity.y < 0 ? Math.abs(i.velocity.y) : i.velocity.y;
 
-        setTimeout(() => controls.start({ y: getNearestValue() }), delay);
+          // if velocity is to heigh, cut it off otherwise state might not be set in time
+          if (velocity > 400) velocity = 400;
+
+          // multiply to make sure you can let it scroll for longer
+          return velocity * 2.5;
+        };
+
+        setTimeout(() => controls.start({ y: getNearestValue() }), delay());
       }}
     >
       {amountOfValues.map((index) => (

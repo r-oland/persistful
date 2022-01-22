3f02471c5739fda163e511cbce2e55049043ef12
Roland Branten <roland.branten@gmail.com>
2022-01-22 17:32:56 +0100

    WIP mobile top nav

--- components/dashboard/TopNav/TopNav.tsx
+++ components/dashboard/TopNav/TopNav.tsx
@@ -1,13 +1,102 @@
 // Components==============
+import { faCalendarDay } from '@fortawesome/pro-regular-svg-icons';
+import { faFlame } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import useGetActiveReward from 'actions/reward/useGetActiveReward';
+import { AnimatePresence, motion } from 'framer-motion';
 import TopNavWrapper from 'global_components/LayoutWrappers/TopNavWrapper/TopNavWrapper';
-import React from 'react';
+import RewardCard from 'global_components/RewardCard/RewardCard';
+import RewardModal from 'global_components/RewardModal/RewardModal';
+import { useOnClickOutside } from 'hooks/useOnClickOutside';
+import React, { useRef, useState } from 'react';
+import { framerFade } from 'utils/framerAnimations';
+import { getMonthString } from 'utils/getMonthString';
 import styles from './TopNav.module.scss';
 // =========================
 
-export default function TopNav() {
+function RewardTooltip({
+  activeReward,
+  setTooltipIsOpen,
+  setModalIsOpen,
+  buttonRef,
+}: {
+  activeReward: RewardEntity;
+  setTooltipIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setModalIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  buttonRef: React.RefObject<HTMLDivElement>;
+}) {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useOnClickOutside({
+    refs: [ref, buttonRef],
+    handler: () => setTooltipIsOpen(false),
+  });
+
   return (
-    <TopNavWrapper>
-      <div className={styles.wrapper} />
-    </TopNavWrapper>
+    <motion.div {...framerFade} className={styles['reward-tooltip']} ref={ref}>
+      <RewardCard reward={activeReward} setModalIsOpen={setModalIsOpen} />
+    </motion.div>
+  );
+}
+
+export default function TopNav() {
+  const today = new Date();
+  const day = today.getDate();
+  const month = getMonthString(today.getMonth());
+  const year = today.getUTCFullYear();
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [rewardModalIsOpen, setRewardModalIsOpen] = useState(false);
+  const [rewardTooltipIsOpen, setRewardTooltipIsOpen] = useState(false);
+
+  const { data: activeReward } = useGetActiveReward();
+
+  return (
+    <>
+      <TopNavWrapper>
+        <div className={styles.wrapper}>
+          <div className={styles.date}>
+            <FontAwesomeIcon icon={faCalendarDay} />
+            <p>
+              {day} {month} {year}
+            </p>
+          </div>
+          <div className={styles.reward}>
+            <div
+              onClick={() => setRewardTooltipIsOpen((prev) => !prev)}
+              ref={ref}
+            >
+              <FontAwesomeIcon icon={faFlame} />
+              <div className={styles.counter}>
+                <p>
+                  {activeReward
+                    ? activeReward.totalCycles - activeReward.completedCycles
+                    : 0}
+                </p>
+              </div>
+            </div>
+            <AnimatePresence>
+              {activeReward && rewardTooltipIsOpen && (
+                <RewardTooltip
+                  activeReward={activeReward}
+                  setTooltipIsOpen={setRewardTooltipIsOpen}
+                  setModalIsOpen={setRewardModalIsOpen}
+                  buttonRef={ref}
+                />
+              )}
+            </AnimatePresence>
+          </div>
+        </div>
+      </TopNavWrapper>
+      <AnimatePresence>
+        {rewardModalIsOpen && activeReward && (
+          <RewardModal
+            setModalIsOpen={setRewardModalIsOpen}
+            reward={activeReward}
+          />
+        )}
+      </AnimatePresence>
+    </>
   );
 }

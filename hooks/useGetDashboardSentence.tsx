ba6bb893697c548c91c203ea22b3684aa1c5d1dc
Roland Branten <roland.branten@gmail.com>
2022-01-23 19:33:58 +0100

    make dashboard sentence dynamic

---
+++ hooks/useGetDashboardSentence.tsx
@@ -0,0 +1,49 @@
+// Components==============
+import useGetUser from 'actions/user/useGetUser';
+import { GlobalTodayStreakContext } from 'global_components/GlobalTodayStreakContextWrapper';
+import { useContext, useEffect, useState } from 'react';
+// =========================
+
+export default function useGetDashboardSentence() {
+  const { data: user } = useGetUser();
+  const { todayStreak } = useContext(GlobalTodayStreakContext);
+
+  const index =
+    todayStreak === 0
+      ? 0
+      : todayStreak > 0 && todayStreak <= 0.99
+      ? 1
+      : todayStreak >= 1 && todayStreak <= 1.99
+      ? 2
+      : todayStreak >= 2 && todayStreak <= 2.99
+      ? 3
+      : todayStreak === 3
+      ? 4
+      : 0;
+
+  const name = user?.firstName || 'person without a name';
+
+  const sentences = [
+    // 0: streak = 0
+    [`Come on ${name} get on it...`, 'Chop chop! Time to get this ball roling'],
+    // 1: streak = 0 -  0.99
+    ['One small step for... oh whatever, keep going!'],
+    // 2:streak = 1 - 1.99
+    ['Hell yeah! Look at you, doing the minimal amount of work.'],
+    // 3: streak = 2 - 2.99
+    [`Holy shit ${name}, you're killing it! 🔪`],
+    // 4: streak = > 3
+    [`Hey hey, I'm proud of you but don't over do it buddy.`],
+  ];
+
+  const array = sentences[index];
+  const i = Math.floor(Math.random() * array.length);
+
+  const [sentence, setSentence] = useState(array[i]);
+
+  useEffect(() => {
+    setSentence(array[i]);
+  }, [index, name]);
+
+  return sentence;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,16 +5,17 @@ import SideBar from 'components/dashboard/SideBar/SideBar';
 import TopNav from 'components/dashboard/TopNav/TopNav';
 import ProgressCircle from 'global_components/ProgressCircle/ProgressCircle';
 import ValidateEffect from 'global_components/ValidateEffect';
+import useGetDashboardSentence from 'hooks/useGetDashboardSentence';
 import { useMediaQ } from 'hooks/useMediaQ';
 import React from 'react';
 // =========================
-
-const sentence = 'Yeez, bit of an overachiever are we?';
 
 export default function Dashboard() {
   const query = useMediaQ('min', 1500);
   // @ts-ignore
   const desktopQuery = useMediaQ('min', 1175);
+
+  const sentence = useGetDashboardSentence();
 
   return (
     <div className={styles.wrapper}>

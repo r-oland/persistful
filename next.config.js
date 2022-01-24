c418f8b8e3abffdf9f48329ea47bdcfd5d344931
Roland Branten <roland.branten@gmail.com>
2022-01-24 19:44:30 +0100

    WIP PWA

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa');
 
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
+  pwa: {
+    dest: 'public/serviceWorker',
+    disable: process.env.NODE_ENV === 'development',
+  },
   images: {
     domains: ['storage.googleapis.com'],
   },
-};
+});

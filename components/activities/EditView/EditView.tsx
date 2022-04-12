a9d490da4e098eb8b64f40daeb363a2045deadfa
Roland Branten <roland.branten@gmail.com>
2022-04-12 20:32:47 +0200

    https://trello.com/c/0ziFVIwP/68-add-confirm-component-for-activities-page

--- components/activities/EditView/EditView.tsx
+++ components/activities/EditView/EditView.tsx
@@ -1,9 +1,11 @@
 // Components==============
 import useGetActivities from 'actions/activity/useGetActivities';
+import { AnimatePresence } from 'framer-motion';
 import ElementContainer from 'global_components/ElementContainer/ElementContainer';
 import { ActivitiesContext } from 'pages/activities';
 import React, { useContext, useState } from 'react';
 import Content from './Content';
+import DeleteConfirmModal from '../DeleteConfirmModal/DeleteConfirmModal';
 import styles from './EditView.module.scss';
 // =========================
 
@@ -18,20 +20,36 @@ export default function EditView() {
     newActivity ? false : activity?.status === 'active'
   );
 
+  const [deleteModalIsOpen, setDeleteModalIsOpen] = useState(false);
+
   const penaltyMode = newActivity ? isToggled : activity?.penalty;
 
+  const handleSwitch = (id: string) => setSelectedActivity(id);
+
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.element}>
-        <ElementContainer color={penaltyMode ? 'red' : 'green'}>
-          <Content
-            isToggled={isToggled}
-            setIsToggled={setIsToggled}
-            activity={activity}
-            handleSwitch={(id) => setSelectedActivity(id)}
-          />
-        </ElementContainer>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.element}>
+          <ElementContainer color={penaltyMode ? 'red' : 'green'}>
+            <Content
+              isToggled={isToggled}
+              setIsToggled={setIsToggled}
+              setDeleteModalIsOpen={setDeleteModalIsOpen}
+              activity={activity}
+              handleSwitch={handleSwitch}
+            />
+          </ElementContainer>
+        </div>
       </div>
-    </div>
+      <AnimatePresence>
+        {deleteModalIsOpen && (
+          <DeleteConfirmModal
+            setModalIsOpen={setDeleteModalIsOpen}
+            activity={activity}
+            handleSwitch={handleSwitch}
+          />
+        )}
+      </AnimatePresence>
+    </>
   );
 }

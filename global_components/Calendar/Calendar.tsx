70a6527a9d90f4c73dddd8412f0dcb225c7197df
Roland Branten <roland.branten@gmail.com>
2022-05-12 19:40:28 +0200

    fix bug where prev date get's displayed

--- global_components/Calendar/Calendar.tsx
+++ global_components/Calendar/Calendar.tsx
@@ -1,4 +1,5 @@
 // Components==============
+import { add } from 'date-fns';
 import { DashboardContext } from 'pages';
 import React, { useContext } from 'react';
 import { DayPicker } from 'react-day-picker';
@@ -8,13 +9,20 @@ import styles from './Calendar.module.scss';
 export default function Calendar() {
   const { activeDay, setActiveDay } = useContext(DashboardContext);
 
+  const handleDayClick = (day: Date) => {
+    const middleOfDay = add(day, { hours: 12 });
+    setActiveDay(middleOfDay);
+  };
+
   return (
     <div className={styles.wrapper}>
       <DayPicker
         selected={activeDay}
-        onSelect={setActiveDay}
+        onDayClick={handleDayClick}
+        required
         toDate={new Date()}
         mode="single"
+        defaultMonth={activeDay}
         weekStartsOn={1}
       />
     </div>

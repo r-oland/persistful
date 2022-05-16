7c7e7d7bc67b57356f8b9c2a4c5892bea2f850dc
Roland Branten <roland.branten@gmail.com>
2022-05-16 20:56:13 +0200

    add second chance to UI

--- global_components/Calendar/Calendar.tsx
+++ global_components/Calendar/Calendar.tsx
@@ -1,4 +1,5 @@
 // Components==============
+import useGetUser from 'actions/user/useGetUser';
 import { add } from 'date-fns';
 import { DashboardContext } from 'pages';
 import React, { useContext } from 'react';
@@ -9,10 +10,16 @@ import styles from './Calendar.module.scss';
 export default function Calendar() {
   const { activeDay, setActiveDay } = useContext(DashboardContext);
 
+  const { data: user } = useGetUser();
+
   const handleDayClick = (day: Date) => {
     const middleOfDay = add(day, { hours: 12 });
     setActiveDay(middleOfDay);
   };
+
+  const modifiers = user?.lastSecondChance
+    ? { secondChance: [new Date(user?.lastSecondChance)] }
+    : undefined;
 
   return (
     <div className={styles.wrapper}>
@@ -24,6 +31,8 @@ export default function Calendar() {
         mode="single"
         defaultMonth={activeDay}
         weekStartsOn={1}
+        modifiers={modifiers}
+        modifiersClassNames={{ secondChance: 'rdp-second_chance' }}
       />
     </div>
   );

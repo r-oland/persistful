fe15ebf066b0d36379a139c3da787c85ec5cca53
Roland Branten <roland.branten@gmail.com>
2022-05-22 10:47:30 +0200

    add exit animation to graph icons && display calcMode times items correctly in graph

--- utils/getActivityCount.ts
+++ utils/getActivityCount.ts
@@ -1,6 +1,11 @@
 import { convertMinutesToHours } from './convertMinutesToHours';
 
-export const getActivityCount = (activity: ActivityEntity) =>
+export const getActivityCount = (
+  activity: ActivityEntity,
+  calcReduction = false
+) =>
   activity.countMode === 'minutes'
     ? convertMinutesToHours(activity.count)
+    : calcReduction
+    ? `${activity.count / activity.countCalc}x`
     : `${Math.floor(activity.count)}x`;

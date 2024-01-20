b4e22caecaf26914db0abd1e4bf7853dc9f4bfd6
Roland Branten <roland.branten@gmail.com>
2024-01-20 22:11:52 +0100

    fix: reverse complement/encouraging texts

--- utils/getNotificationMessage.ts
+++ utils/getNotificationMessage.ts
@@ -1,22 +1,5 @@
-// Complementing Titles and Messages (for not reaching daily goal)
+// Complementing Titles and Messages (for reaching daily goal)
 const complementingTitles = [
-  'Oopsie-Daisy!',
-  'Habit Hibernation!',
-  'Goal MIA Alert!',
-  'Lost in Habit-land!',
-  'Habit Whisperer Needed!',
-];
-
-const complementingMessages = [
-  "Looks like we're still missing some habit love today. Ready to check-in and turn the day around?",
-  'Your habit might be taking a little nap. Time to wake it up with a quick check-in!',
-  'Your daily goal seems to be playing hide and seek. Ready to track it down and mark it off?',
-  'Your habit might be exploring new territories. Give it a nudge with a check-in and guide it back!',
-  'Your habit is being a bit shy today. Show it some love by logging your progress!',
-];
-
-// Encouraging Titles and Messages (for reaching daily goal)
-const encouragingTitles = [
   'Goal Unlocked!',
   'Habit Hero!',
   'Habit Masterclass!',
@@ -24,12 +7,29 @@ const encouragingTitles = [
   'Goal Crusher Communique!',
 ];
 
-const encouragingMessages = [
+const complementingMessages = [
   'Boom! You smashed your daily habit goal. Treat yourself to a victory dance!',
   'You did it! Consider your habit conquered for the day. High-fives all around!',
   "Ding ding ding! You've officially mastered today's habit challenge. Ready for an encore tomorrow?",
   'Gold star alert! You not only met but conquered your habit goal. Shine on, superstar!',
   'Congratulations! Your habit goal has officially been crushed. Time to celebrate your triumph!',
+];
+
+// Encouraging Titles and Messages (for not reaching daily goal)
+const encouragingTitles = [
+  'Oopsie-Daisy!',
+  'Habit Hibernation!',
+  'Goal MIA Alert!',
+  'Lost in Habit-land!',
+  'Habit Whisperer Needed!',
+];
+
+const encouragingMessages = [
+  "Looks like we're still missing some habit love today. Ready to check-in and turn the day around?",
+  'Your habit might be taking a little nap. Time to wake it up with a quick check-in!',
+  'Your daily goal seems to be playing hide and seek. Ready to track it down and mark it off?',
+  'Your habit might be exploring new territories. Give it a nudge with a check-in and guide it back!',
+  'Your habit is being a bit shy today. Show it some love by logging your progress!',
 ];
 
 const getRandomText = (texts: string[]) =>

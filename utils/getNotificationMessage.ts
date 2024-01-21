b75384216d15b45b6713c814875f9370b799e441
Roland Branten <roland.branten@gmail.com>
2024-01-21 14:49:33 +0100

    chore: add more notification messages

--- utils/getNotificationMessage.ts
+++ utils/getNotificationMessage.ts
@@ -2,9 +2,13 @@
 const complementingTitles = [
   'Goal Unlocked!',
   'Habit Hero!',
-  'Habit Masterclass!',
   'Habit Superstar!',
-  'Goal Crusher Communique!',
+  'Goal Crusher!',
+  'Habit Ninja Strikes Again!',
+  'Daily Goal Dominated!',
+  'Habit Overlord at Work!',
+  'Goal Gladiator Triumphs!',
+  'Habit Whiz!',
 ];
 
 const complementingMessages = [
@@ -13,6 +17,12 @@ const complementingMessages = [
   "Ding ding ding! You've officially mastered today's habit challenge. Ready for an encore tomorrow?",
   'Gold star alert! You not only met but conquered your habit goal. Shine on, superstar!',
   'Congratulations! Your habit goal has officially been crushed. Time to celebrate your triumph!',
+  'Another day, another habit conquered. You are the true Habit Ninja!',
+  'Daily goal, consider yourself officially dominated. Ready for tomorrow’s challenge?',
+  'Witness the power of the Habit Overlord! Your daily goal is no match for your prowess!',
+  "Goal Gladiator, you've triumphed again! The habit arena is yours for the taking.",
+  "In the Chronicles of Habit, today's entry is all about your epic conquest. Well done!",
+  "You’re a Habit Maestro! Today's performance is worthy of a standing ovation.",
 ];
 
 // Encouraging Titles and Messages (for not reaching daily goal)
@@ -22,6 +32,9 @@ const encouragingTitles = [
   'Goal MIA Alert!',
   'Lost in Habit-land!',
   'Habit Whisperer Needed!',
+  'Goal Resilience Reminder!',
+  'Daily Goal, Round Two!',
+  'Quest for the Goal Continues!',
 ];
 
 const encouragingMessages = [
@@ -30,6 +43,7 @@ const encouragingMessages = [
   'Your daily goal seems to be playing hide and seek. Ready to track it down and mark it off?',
   'Your habit might be exploring new territories. Give it a nudge with a check-in and guide it back!',
   'Your habit is being a bit shy today. Show it some love by logging your progress!',
+  'The quest for the goal continues! Your dedication is the key to habit triumphs.',
 ];
 
 const getRandomText = (texts: string[]) =>

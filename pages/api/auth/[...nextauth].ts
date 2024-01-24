336c034ebac9188343e0a1298cebb36fed00f18b
Roland Branten <roland.branten@gmail.com>
2024-01-24 11:32:21 +0100

    feat: allow account linking with Google auth provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -16,6 +16,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
     EmailProvider({
       server: {

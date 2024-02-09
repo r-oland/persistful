a53c2f91ab9cd8ad08b84f349e17067987c066c8
Roland Branten <roland.branten@gmail.com>
2024-02-09 14:33:56 +0100

    chore: Move active reward to front in openRewards API

--- pages/api/reward/openRewards.ts
+++ pages/api/reward/openRewards.ts
@@ -1,6 +1,25 @@
+import { ObjectId } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { checkAuth } from 'utils/checkAuth';
 import { getCollection } from 'utils/getMongo';
+
+function moveActiveRewardToFront(
+  rewards: RewardEntity[],
+  activeRewardId?: string
+) {
+  if (!activeRewardId) return rewards;
+
+  const activeReward = rewards.find(
+    (r) => r._id?.toString() === activeRewardId?.toString()
+  );
+
+  if (activeReward) {
+    const index = rewards.indexOf(activeReward);
+    rewards.splice(index, 1);
+    rewards.unshift(activeReward);
+  }
+  return rewards;
+}
 
 export default async function handler(
   req: NextApiRequest,
@@ -11,6 +30,13 @@ export default async function handler(
     if (!session) return;
 
     if (req.method === 'GET') {
+      // Get user id
+      const _id = new ObjectId(session.user.uid) as any;
+
+      // get user
+      const users = await getCollection<UserEntity>('users');
+      const user = await users.findOne({ _id });
+
       // get open rewards (rewards that are not claimed)
       const rewards = await getCollection<RewardEntity>('rewards');
       const openRewards = await rewards
@@ -20,7 +46,8 @@ export default async function handler(
           endDate: { $exists: false },
         })
         .sort({ createdAt: -1 })
-        .toArray();
+        .toArray()
+        .then((r) => moveActiveRewardToFront(r, user?.activeReward));
 
       // if they doen't exist, return nothing
       if (!openRewards.length) return res.status(200).send([]);

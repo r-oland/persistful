fd1f96ecbae73e0985cdad509dfcf912cbd20e69
Roland Branten <roland.branten@gmail.com>
2024-03-10 10:48:23 +0100

    fix: correct second chance handling in getStreakDays

--- utils/validateStreaks/getStreakDays.ts
+++ utils/validateStreaks/getStreakDays.ts
@@ -4,10 +4,13 @@ import { validateStreaksArgs } from 'pages/api/user/validateStreaks';
 
 export function getStreakDays({ days, user }: validateStreaksArgs) {
   const secondChance = user.rules.secondChange;
-  let missedChanceThisWeek = false;
   let currentWeek = getWeek(days[0].createdAt, { weekStartsOn: 1 });
   const streakDays: DayEntity[] = [];
   const secondChanceDates: Date[] = [];
+  const getSecondChanceDatesInLastWeek = (week: number) =>
+    secondChanceDates.filter(
+      (date) => getWeek(date, { weekStartsOn: 1 }) === week
+    );
 
   for (let i = 0; i < days.length; i++) {
     const day = days[i];
@@ -15,7 +18,29 @@ export function getStreakDays({ days, user }: validateStreaksArgs) {
 
     // Reset the second chance if it's a new week
     if (weekNumber !== currentWeek) {
-      missedChanceThisWeek = false;
+      const secondChanceDatesInLastWeek =
+        getSecondChanceDatesInLastWeek(currentWeek);
+
+      // There should never be more then 1 second chance date in a week -> make corrections in this week and break loop
+      if (secondChanceDatesInLastWeek.length > 1) {
+        // clear second chance dates for this week
+        const secondChanceDateIndex = secondChanceDates.findIndex(
+          (sd) => sd === secondChanceDatesInLastWeek[0]
+        );
+        secondChanceDates.splice(secondChanceDateIndex);
+        //
+
+        // clear streak days for this week starting from the last second chance date
+        const streakDayIndex = streakDays.findIndex(
+          (sd) =>
+            sd.createdAt.getTime() < secondChanceDatesInLastWeek[0].getTime()
+        );
+        streakDays.splice(streakDayIndex);
+        //
+
+        break;
+      }
+
       currentWeek = weekNumber;
     }
 
@@ -30,10 +55,7 @@ export function getStreakDays({ days, user }: validateStreaksArgs) {
 
       // gap 2 = 1 missed day
       if (gap === 2) {
-        if (secondChance && !missedChanceThisWeek) {
-          // Use the second chance for this week
-          missedChanceThisWeek = true;
-
+        if (secondChance) {
           // This is the day before the missed day, so push 1 day ahead to the second chance dates
           secondChanceDates.push(add(day.createdAt, { days: 1 }));
         } else {
@@ -46,26 +68,14 @@ export function getStreakDays({ days, user }: validateStreaksArgs) {
     // Check if the day contributes to the streak
     if (getDayAchievements(day).streak) {
       streakDays.push(day);
-    } else if (secondChance && !missedChanceThisWeek) {
+    } else if (secondChance) {
       // Use the second chance for this week
-      missedChanceThisWeek = true;
-      streakDays.push(day);
       secondChanceDates.push(day.createdAt);
     } else {
       // The streak is broken
       break;
     }
   }
-
-  // Make sure that the first day in streak is not a second chance day
-  const startDate =
-    streakDays[streakDays.length - 1].createdAt || streakDays[0].createdAt;
-
-  if (streakDays.length && secondChanceDates.includes(startDate)) {
-    streakDays.pop();
-    secondChanceDates.pop();
-  }
-  //
 
   return { streakDays, secondChanceDates };
 }

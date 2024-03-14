ebfdf9904dbd066cbad44bce1a9531be3b6c122e
Roland Branten <roland.branten@gmail.com>
2024-03-14 11:29:50 +0100

    chore: refactor LineGraph component to use separate render methods

--- components/progress/ActivityLineGraph/LineGraph/LineGraph.tsx
+++ components/progress/ActivityLineGraph/LineGraph/LineGraph.tsx
@@ -3,24 +3,30 @@ import * as d3 from 'd3';
 import { useDimensions } from 'hooks/useDimensions';
 import styles from './LineGraph.module.scss';
 import { ActivityLineGraphContext } from '../ActivityLineGraph';
+import { renderAxes, renderCircles, renderLine } from './renderMethods';
 
 const MARGIN = { top: 30, right: 30, bottom: 30, left: 40 };
 
-type DataPoint = { x: number; y: number };
+export default function LineGraph() {
+  const wrapperRef = useRef<HTMLDivElement>(null);
 
-function LineGraph({ width, height }: { width: number; height: number }) {
+  const { activities } = useContext(ActivityLineGraphContext);
   const { daysSum } = useContext(ActivityLineGraphContext);
+
+  const { width, height } = useDimensions({ current: wrapperRef.current }, [
+    activities.length,
+  ]);
 
   const data = daysSum.map((day, index) => ({
     x: index,
     y: day.sum,
   }));
 
-  const circleRef = useRef(null);
-  const lineRef = useRef(null);
+  const circleRef = useRef<SVGSVGElement | null>(null);
+  const lineRef = useRef<SVGSVGElement | null>(null);
 
   // bounds = area inside the graph axis = calculated by subtracting the margins
-  const axesRef = useRef(null);
+  const axesRef = useRef<SVGSVGElement | null>(null);
   const boundsWidth = width - MARGIN.right - MARGIN.left;
   const boundsHeight = height - MARGIN.top - MARGIN.bottom;
 
@@ -49,104 +55,34 @@ function LineGraph({ width, height }: { width: number; height: number }) {
     [data, width]
   );
 
-  // Render the Y axis using d3.js, not react
   useEffect(() => {
-    const svgElement = d3.select(axesRef.current);
-    svgElement.selectAll('*').remove();
-
-    const circleElement = d3.select(circleRef.current);
-    circleElement.selectAll('*').remove();
-
-    const lineElement = d3.select(lineRef.current);
-    lineElement.selectAll('*').remove();
-
-    // Build the line
-    const lineBuilder = d3
-      .line<DataPoint>()
-      .x((d) => xScale(d.x))
-      .y((d) => yScale(d.y));
-
-    const linePath = lineBuilder(data);
-
-    // Render the line
-    lineElement
-      .append('path')
-      .attr('d', linePath)
-      .attr('opacity', 1)
-      .attr('stroke', '#18E597')
-      .attr('fill', 'none')
-      .attr('strokeWidth', 1);
-
-    const yAxisGenerator = d3
-      .axisLeft(yScale)
-      .tickValues(d3.range(0, yAxisMax + 60, 60)) // Set tick values to every hour
-      .tickSize(-boundsWidth)
-      .tickFormat((d) => `${Math.floor(Number(d) / 60)}h`);
-
-    const yAxis = svgElement.append('g').call(yAxisGenerator);
-
-    // Move the text slightly to the left and change the fill of the text
-    yAxis
-      .selectAll('text')
-      .attr('x', -8) // Move the text 10 units to the left
-      .attr('fill', '#A8ACB1'); // Change the fill of the text to #A8ACB1
-
-    // Change tick color
-    yAxis.selectAll('line').attr('stroke', '#f8f8f9');
-
-    // Prevent a large amount of circles from being rendered
-    if (daysSum.length < width / 6) {
-      // Render the data points
-      circleElement
-        .selectAll('circle')
-        .data(data)
-        .enter()
-        .append('circle')
-        .attr('cx', (d) => xScale(d.x))
-        .attr('cy', (d) => yScale(d.y))
-        .attr('r', 2) // Set the radius of the circles
-        .attr('fill', '#E8FCF5')
-        .attr('stroke', '#282F36');
-    }
-
-    yAxis.select('.domain').remove(); // Remove the axis path
+    renderAxes(axesRef, yScale, boundsWidth, yAxisMax);
+    renderLine(lineRef, data, xScale, yScale);
+    renderCircles(circleRef, data, xScale, yScale, daysSum, width);
   }, [yScale, boundsHeight]);
 
   return (
-    <svg width={width} height={height} className={styles.graph}>
-      <g
-        width={boundsWidth}
-        height={boundsHeight}
-        ref={axesRef}
-        transform={`translate(${[MARGIN.left, MARGIN.top].join(',')})`}
-      />
-      <g
-        width={boundsWidth}
-        height={boundsHeight}
-        transform={`translate(${[MARGIN.left, MARGIN.top].join(',')})`}
-        ref={lineRef}
-      />
-      <g
-        ref={circleRef}
-        transform={`translate(${[MARGIN.left, MARGIN.top].join(',')})`}
-        width={boundsWidth}
-        height={boundsHeight}
-      />
-    </svg>
-  );
-}
-
-export default function LineGraphWrapper() {
-  const wrapperRef = useRef<HTMLDivElement>(null);
-  const { activities } = useContext(ActivityLineGraphContext);
-
-  const { width, height } = useDimensions({ current: wrapperRef.current }, [
-    activities.length,
-  ]);
-
-  return (
     <div ref={wrapperRef} className={styles.wrapper}>
-      {!!(width && height) && <LineGraph width={width} height={height} />}
+      <svg width={width} height={height} className={styles.graph}>
+        <g
+          width={boundsWidth}
+          height={boundsHeight}
+          ref={axesRef}
+          transform={`translate(${[MARGIN.left, MARGIN.top].join(',')})`}
+        />
+        <g
+          width={boundsWidth}
+          height={boundsHeight}
+          ref={lineRef}
+          transform={`translate(${[MARGIN.left, MARGIN.top].join(',')})`}
+        />
+        <g
+          width={boundsWidth}
+          height={boundsHeight}
+          ref={circleRef}
+          transform={`translate(${[MARGIN.left, MARGIN.top].join(',')})`}
+        />
+      </svg>
     </div>
   );
 }
---
+++ components/progress/ActivityLineGraph/LineGraph/renderMethods.ts
@@ -0,0 +1,86 @@
+import * as d3 from 'd3';
+
+type DataPoint = { x: number; y: number };
+
+export function renderAxes(
+  axesRef: React.RefObject<SVGSVGElement>,
+  yScale: d3.ScaleLinear<number, number>,
+  boundsWidth: number,
+  yAxisMax: number
+) {
+  const axesElement = d3.select(axesRef.current);
+  axesElement.selectAll('*').remove();
+
+  const yAxisGenerator = d3
+    .axisLeft(yScale)
+    .tickValues(d3.range(0, yAxisMax + 60, 60)) // Set tick values to every hour
+    .tickSize(-boundsWidth)
+    .tickFormat((d) => `${Math.floor(Number(d) / 60)}h`);
+
+  const yAxis = axesElement.append('g').call(yAxisGenerator);
+
+  // Move the text slightly to the left and change the fill of the text
+  yAxis
+    .selectAll('text')
+    .attr('x', -8) // Move the text 10 units to the left
+    .attr('fill', '#A8ACB1'); // Change the fill of the text to #A8ACB1
+
+  // Change tick color
+  yAxis.selectAll('line').attr('stroke', '#f8f8f9');
+
+  yAxis.select('.domain').remove(); // Remove the axis path
+}
+
+export function renderLine(
+  lineRef: React.RefObject<SVGSVGElement>,
+  data: DataPoint[],
+  xScale: d3.ScaleLinear<number, number>,
+  yScale: d3.ScaleLinear<number, number>
+) {
+  const lineElement = d3.select(lineRef.current);
+  lineElement.selectAll('*').remove();
+
+  // Build the line
+  const lineBuilder = d3
+    .line<DataPoint>()
+    .x((d) => xScale(d.x))
+    .y((d) => yScale(d.y));
+
+  const linePath = lineBuilder(data);
+
+  // Render the line
+  lineElement
+    .append('path')
+    .attr('d', linePath)
+    .attr('opacity', 1)
+    .attr('stroke', '#18E597')
+    .attr('fill', 'none')
+    .attr('strokeWidth', 1);
+}
+
+export function renderCircles(
+  circleRef: React.RefObject<SVGSVGElement>,
+  data: DataPoint[],
+  xScale: d3.ScaleLinear<number, number>,
+  yScale: d3.ScaleLinear<number, number>,
+  daysSum: { sum: number; date: Date }[],
+  width: number
+) {
+  const circleElement = d3.select(circleRef.current);
+  circleElement.selectAll('*').remove();
+
+  // Prevent a large amount of circles from being rendered
+  if (daysSum.length < width / 6) {
+    // Render the data points
+    circleElement
+      .selectAll('circle')
+      .data(data)
+      .enter()
+      .append('circle')
+      .attr('cx', (d) => xScale(d.x))
+      .attr('cy', (d) => yScale(d.y))
+      .attr('r', 2) // Set the radius of the circles
+      .attr('fill', '#E8FCF5')
+      .attr('stroke', '#282F36');
+  }
+}

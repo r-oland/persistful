dffcb7f8f945c24e906ca3524f5efa1def5d2185
Roland Branten <roland.branten@gmail.com>
2024-04-04 20:49:08 +0200

    fix: bug where return statement canceled entire handler

--- pages/api/reward/updateStreak.ts
+++ pages/api/reward/updateStreak.ts
@@ -1,7 +1,76 @@
 import { ObjectId } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Session } from 'next-auth';
 import { checkAuth } from 'utils/checkAuth';
 import { getCollection } from 'utils/getMongo';
+
+const updateActiveReward = async (
+  session: Session,
+  newStreak: number,
+  oldStreak: number
+) => {
+  // Get user
+  const userId = new ObjectId(session.user.uid) as any;
+  const users = await getCollection<UserEntity>('users');
+  const user = await users.findOne({ _id: userId });
+
+  // Nothing to update if user has no active reward
+  if (!user?.activeReward) return false;
+
+  // get rewards
+  const rewards = await getCollection<RewardEntity>('rewards');
+
+  const reward = await rewards.findOne({ _id: user.activeReward });
+  if (!reward) return false;
+
+  const getStreakDiffForStreakMode = () => {
+    const streakDiff = newStreak - oldStreak;
+
+    // Not in streak mode, ignore and return streakDiff
+    if (!reward.minCycles || reward.mode !== 'streak') return streakDiff;
+
+    const currentDailyStreak = (user.streak || 0) + newStreak;
+    const dailyStreakBeforeUpdate = currentDailyStreak - streakDiff;
+
+    // user has corrected previous input
+    if (streakDiff < 0) return streakDiff;
+    // Current streak was already greater then minCycles
+    if (dailyStreakBeforeUpdate >= reward.minCycles) return streakDiff;
+    // new daily streak is not high enough to satisfy minCycles
+    if (reward.minCycles > currentDailyStreak) return 0;
+    // daily streak was exactly matched with minCycles in this update
+    if (currentDailyStreak === reward.minCycles) return 1;
+    // daily streak was satisfied in this update, calculate how many cycles were completed since minCycles was satisfied
+    if (currentDailyStreak > reward.minCycles)
+      return currentDailyStreak + 1 - reward.minCycles;
+
+    // Fallback
+    return streakDiff;
+  };
+
+  const updatedCompletedCycles =
+    reward.completedCycles + getStreakDiffForStreakMode();
+
+  const completedCycles =
+    updatedCompletedCycles < 0
+      ? // Prevent negative values
+        0
+      : updatedCompletedCycles > reward.totalCycles
+        ? // Prevent values higher than totalCycles
+          reward.totalCycles
+        : updatedCompletedCycles;
+
+  // Update active reward with newly calculated completedCycles
+  await rewards.findOneAndUpdate(
+    { _id: user.activeReward },
+    { $set: { completedCycles } }
+  );
+
+  // Return whether reward was completed in this update to trigger modal in frontend
+  if (completedCycles === reward.totalCycles) return true;
+
+  return false;
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -20,67 +89,14 @@ export default async function handler(
 
       // If streak has changed, update active reward
       if (oldStreak !== newStreak) {
-        // Get user
-        const userId = new ObjectId(session.user.uid) as any;
-        const users = await getCollection<UserEntity>('users');
-        const user = await users.findOne({ _id: userId });
-
-        // Nothing to update if user has no active reward
-        if (!user?.activeReward) return;
-
-        // get rewards
-        const rewards = await getCollection<RewardEntity>('rewards');
-
-        const reward = await rewards.findOne({ _id: user.activeReward });
-        if (!reward) return;
-
-        const getStreakDiffForStreakMode = () => {
-          const streakDiff = newStreak - oldStreak;
-
-          // Not in streak mode, ignore and return streakDiff
-          if (!reward.minCycles || reward.mode !== 'streak') return streakDiff;
-
-          const currentDailyStreak = (user.streak || 0) + newStreak;
-          const dailyStreakBeforeUpdate = currentDailyStreak - streakDiff;
-
-          // user has corrected previous input
-          if (streakDiff < 0) return streakDiff;
-          // Current streak was already greater then minCycles
-          if (dailyStreakBeforeUpdate >= reward.minCycles) return streakDiff;
-          // new daily streak is not high enough to satisfy minCycles
-          if (reward.minCycles > currentDailyStreak) return 0;
-          // daily streak was exactly matched with minCycles in this update
-          if (currentDailyStreak === reward.minCycles) return 1;
-          // daily streak was satisfied in this update, calculate how many cycles were completed since minCycles was satisfied
-          if (currentDailyStreak > reward.minCycles)
-            return currentDailyStreak + 1 - reward.minCycles;
-
-          // Fallback
-          return streakDiff;
-        };
-
-        const updatedCompletedCycles =
-          reward.completedCycles + getStreakDiffForStreakMode();
-
-        const completedCycles =
-          updatedCompletedCycles < 0
-            ? // Prevent negative values
-              0
-            : updatedCompletedCycles > reward.totalCycles
-              ? // Prevent values higher than totalCycles
-                reward.totalCycles
-              : updatedCompletedCycles;
-
-        // Update active reward with newly calculated completedCycles
-        await rewards.findOneAndUpdate(
-          { _id: user.activeReward },
-          { $set: { completedCycles } }
+        const didCompleteReward = await updateActiveReward(
+          session,
+          newStreak,
+          oldStreak
         );
 
-        // Return whether reward was completed in this update to trigger modal in frontend
-        if (completedCycles === reward.totalCycles) completedReward = true;
+        completedReward = didCompleteReward;
       }
-
       res.status(200).send({ completedReward });
     }
   } catch (err: any) {
